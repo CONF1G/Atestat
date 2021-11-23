5c35b50805d8544b1b85afe4f5c25beccf65a453
CONF1G <61599564+CONF1G@users.noreply.github.com>
2021-11-24 01:21:57 +0200

    Update problema 1.cpp

--- exercitiu 1/problema 1.cpp
+++ exercitiu 1/problema 1.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+fstream fin("sir.in"); 
+fstream fout("sir.out");
 
 int main()
 {
     int x,a[101],b[101],i,n;//initializare
     bool ok=0; //initializare verificare introducere nr in sir
-    cin>>n; //numarul de elemente din vector
+    fin>>n; //numarul de elemente din vector
     
     for(i=1; i<=n; i++)
     {
         cin>>a[i]; //elementele din vector
         b[i]=a[i]; //copie vector
     }
-    cin>>x; //citire nr
+    fin>>x; //citire nr
     for(i=1; i<=n+1; i++)
     {
         if(a[i]>x && ok!=1)// verificare nr si introducerea lui in sir 
@@ -30,7 +33,7 @@ int main()
 
 for(i=1; i<=n+1; i++)
 {
-    cout<<a[i]<<" "; // afisarea vectorului
+    fout<<a[i]<<" "; // afisarea vectorului
 
 }
 return 0;
