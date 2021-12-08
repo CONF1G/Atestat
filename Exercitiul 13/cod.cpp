82a37c98b842fca50bc39ebdb5d2e6f24db11fb2
CONF1G <61599564+CONF1G@users.noreply.github.com>
2021-12-08 09:03:04 +0200

    Create cod.cpp

---
+++ Exercitiul 13/cod.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream fin("numere.in");
+int a[100];
+int main()
+{
+    int n,x,p,i,cifra,nr;
+
+    fin>>n;
+    for(i=1; i<=n; i++)
+    {
+        c
+    fin>>nr;
+
+        a[nr]++;
+    }
+    int maxx=0;
+    for(nr=1; nr<100; nr++)
+    {
+        if(maxx<a[nr])
+        {
+            maxx=a[nr];
+            p=nr;
+        }
+    }
+
+    cout<<"Numărul care apare cel mai des este"<<" "<<p;
+
+    return 0;
+}
