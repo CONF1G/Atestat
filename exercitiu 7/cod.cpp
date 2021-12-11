e455f97f069d7dd626ce898f7b42fae950dfcc0e
CONF1G <61599564+CONF1G@users.noreply.github.com>
2021-12-11 09:39:13 +0200

    Update cod.cpp

--- exercitiu 7/cod.cpp
+++ exercitiu 7/cod.cpp
@@ -5,7 +5,43 @@ using namespace std;
 
 int main()
 {
+    int i,j;#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
     int i,j;
+    bool ok=1;
+    char a[256],lit,litr;
+    cin>>a;
+
+    for(i=0; a[i]!=NULL; i++)
+    {
+        lit=a[i];
+        for(j=0; a[j]!=NULL; j++)
+        {
+            ok=1;
+            if(a[j]==lit && j!=i)
+            {
+                ok=0;
+                break;
+            }
+        }
+        if(ok!=0)
+        {
+            litr=lit;
+            break;
+        }
+    }
+    if(ok==0)
+        cout<<"NU EXISTA";
+    else
+        cout<<litr;
+    return 0;
+}
+
     bool ok=1;
     char a[256],lit,litr;
     cin>>a;
