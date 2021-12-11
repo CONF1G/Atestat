7fa4a4edd5d84c435f46348f5a9fe7c54d150250
CONF1G <61599564+CONF1G@users.noreply.github.com>
2021-12-11 09:52:22 +0200

    Create cod.cpp

---
+++ Exercitiu 17/cod.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+fstream fin("matrice.in");
+
+int main()
+{
+    int n,a[101][101],sum=0,i,j;
+
+    cin>>n;
+
+    for(i=1; i<=n; i++)
+        for(j=1; j<=n; j++)
+            cin>>a[i][j];
+
+
+    for(i=1; i<=n; i++)
+        for(j=1; j<=n; j++)
+            if(i + j > n + 1)
+            {
+                sum+=a[i][j];
+            }
+
+    cout<<sum;
+
+
+    return 0;
+}
